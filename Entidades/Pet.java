5b0c13a20f6f8afda94821291be86ac4bb10d6ff
Luis5Felipe <luisfelipedos29@gmail.com>
2025-05-02 22:25:17 -0300

    feat: Construção de algumas regras de negocio do objeto pet

--- Entidades/Pet.java
+++ Entidades/Pet.java
@@ -1,5 +1,84 @@
-package Services;
+package Entidades;
+
+import Utils.SexoPet;
+import Utils.TipoAnimal;
 
 public class Pet {
-    private String nomeEsobrenome;
+    private String nome;
+    private SexoPet sexoPet;
+    private TipoAnimal tipoAnimal;
+    private String[] endereco;
+    private int idade;
+    private double peso;
+    private String race;
+
+    public Pet() {
+
+    }
+
+    public Pet(String[] endereco, int idade, String nome, double peso, String race, SexoPet sexoPet, TipoAnimal tipoAnimal) {
+        this.endereco = endereco;
+        this.idade = idade;
+        this.nome = nome;
+        this.peso = peso;
+        this.race = race;
+        this.sexoPet = sexoPet;
+        this.tipoAnimal = tipoAnimal;
+    }
+
+    public String[] getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String[] endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public SexoPet getSexoPet() {
+        return sexoPet;
+    }
+
+    public void setSexoPet(SexoPet sexoPet) {
+        this.sexoPet = sexoPet;
+    }
+
+    public TipoAnimal getTipoAnimal() {
+        return tipoAnimal;
+    }
+
+    public void setTipoAnimal(TipoAnimal tipoAnimal) {
+        this.tipoAnimal = tipoAnimal;
+    }
 }
